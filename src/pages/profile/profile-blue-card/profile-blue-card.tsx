cdee6e9f49ac2f06782e51f6de1bfd55a801015f
Igor Pavetka <uropb.86@gmail.com>
2023-03-25 17:30:43 +0300

    feat: make profile-blue-card universal component like blue and red card

--- src/pages/profile/profile-blue-card/profile-blue-card.tsx
+++ src/pages/profile/profile-blue-card/profile-blue-card.tsx
@@ -1,12 +1,26 @@
+import classNames from 'classnames';
+
 import style from './profile-blue-card.module.css';
 
 export type ProfileBlueCardType = {
     text: string;
+    type?: string,
+    comment?: string
 }
-export const ProfileBlueCard = ({ text } : ProfileBlueCardType) => (
-    <div className={style.wrapper} data-test-id='empty-blue-card'>
-        <p className={style.text}>
-            {text}
-        </p>
+export const ProfileBlueCard = ({ type = 'blue', text, comment } : ProfileBlueCardType) => (
+    <div 
+        className={classNames(
+            style.wrapper,
+            {
+                [style.wrapper_blue_card]: type === 'blue',
+                [style.wrapper_red_card]: type === 'red',
+            }
+        )}
+        data-test-id={type === 'blue' ? 'empty-blue-card' : 'expired'}
+    >
+        <p className={style.text}>{text}</p>
+        {comment && 
+            <p className={style.comment}>{comment}</p>
+        }
     </div>
 )
